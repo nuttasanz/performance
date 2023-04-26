c6ac6f143f5e9581eaebc72b96ea0c2b537c9369
Froy <nuttasan.k@topcloud.co>
2023-04-26 17:51:21 +0700

    fix bugs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
   css: [
     "~/assets/css/tailwind.css",
-    "~/assets/css/element/input.css",
     "~/assets/css/font.css",
     "~/assets/css/main.css",
   ],
